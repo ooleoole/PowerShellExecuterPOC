c3ff1257ba6a7ac3caeeb63565c70e2845c4f1ea
Ola Popp <olapopp85@gmail.com>
2017-09-01 12:52:11 +0200

    Add project files.

---
+++ PowerShellExecuterTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace PowerShellExecuterTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var powerShellInstance = PowerShell.Create())
+            {
+                var runner = new ScriptRunner();
+                runner.ExecuteScript("param($param1) $d = get-date; $s = 'test string value'; " +
+                                     "$d; $s; $param1; get-service", new PsParameter { ParameterName = "param1", ParameterData = "parameter 1 value metod" });
+
+                runner.ExecuteScriptAsync("$s1 = 'test1'; $s2 = 'test2'; $s1; write-error 'some error';start-sleep -s 7; $s2");
+               
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
---
+++ PowerShellExecuterTest/ScriptRunner.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Management.Automation;
+using System.Threading;
+
+namespace PowerShellExecuterTest
+{
+    public sealed class ScriptRunner
+    {
+        public void ExecuteScript(string script, params PsParameter[] parameters)
+        {
+            using (var powerShellInstance = PowerShell.Create())
+            {
+                powerShellInstance.AddScript(script);
+                foreach (var parameter in parameters)
+                    powerShellInstance.AddParameter(parameter.ParameterName, parameter.ParameterData);
+
+                var output = powerShellInstance.Invoke();
+                if (powerShellInstance.Streams.Error.Count > 0)
+                {
+                    //TODO:Do something
+                }
+                OutputValue(output);
+            }
+        }
+
+        public void ExecuteScriptAsync(string script, params PsParameter[] parameters)
+        {
+            using (var powerShellInstance = PowerShell.Create())
+            {
+                powerShellInstance.AddScript(script);
+                if (parameters != null)
+                    foreach (var parameter in parameters)
+                        powerShellInstance.AddParameter(parameter.ParameterName, parameter.ParameterData);
+
+                var output = new PSDataCollection<PSObject>();
+
+                output.DataAdded += Output_DataAdded;
+                powerShellInstance.Streams.Error.DataAdded += Error_DataAdded;
+
+                var result = powerShellInstance.BeginInvoke<PSObject, PSObject>(null, output);
+
+                while (!result.IsCompleted)
+                {
+                    Console.WriteLine("Waiting for pipeline to finish...");
+                    Thread.Sleep(1000);
+
+
+                }
+
+                Console.WriteLine("Execution has stopped. The pipeline state: " + powerShellInstance.InvocationStateInfo.State);
+                OutputValue(output);
+            }
+        }
+
+        private void Error_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            Console.WriteLine("An error was written to the Error stream!");
+        }
+
+        private void Output_DataAdded(object sender, DataAddedEventArgs e)
+        {
+            Console.WriteLine($"Object:{sender.GetType()} added something to output");
+        }
+
+        private static void OutputValue(IEnumerable<PSObject> output)
+        {
+            foreach (var psObject in output)
+            {
+                if (psObject.BaseObject.GetType().ToString()!="System.ServiceProcess.ServiceController")
+                {
+                    Console.WriteLine(psObject.BaseObject.GetType().FullName);
+                    Console.WriteLine(psObject.BaseObject + "\n");
+                }
+               
+            }
+        }
+    }
+
+    public class PsParameter
+    {
+        public string ParameterName { get; set; }
+        public string ParameterData { get; set; }
+
+    }
+}
